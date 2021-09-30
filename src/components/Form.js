2ad49a20bc523a8a34db2b51b56d4e889badd7d5
chamindujs <chamindujs@gmail.com>
2021-10-01 01:53:58 +0530

    Make the website responsive

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,11 +20,14 @@ const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
 
     return(
         <form>
-            <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+            <div className="formm">
+                <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
             <button onClick={submitTodoHandler} className="todo-button" type="submit">
                 <i className="fas fa-plus-square"></i>
 
             </button>
+            </div>
+            
             <div className="select">
                 <select onChange={statusHandler} name="todos" className="filter-todo">
                     <option value="all">All</option>
